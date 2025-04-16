030f4f4c0993e9718bd6d35da63b313f16f33575
MoonHo <75512752+monho@users.noreply.github.com>
2025-04-16 17:16:44 +0900

    feat : 위치 서비스 약관 체크박스 변경 함수 구현

--- lib/viewmodel/terms/termsViewModel.dart
+++ lib/viewmodel/terms/termsViewModel.dart
@@ -10,13 +10,19 @@ class Termsviewmodel extends ChangeNotifier {
   bool get gpsTerms => _gpsTerms;
   String? get errorMessage => _errorMessage;
 
-
   //전체 동의 체크박스 상태변경
-  void toggleAllAgreed(bool? value){
+  void toggleAllAgreed(bool? value) {
     _allAgreed = value ?? false;
     _gpsTerms = _allAgreed;
     _errorMessage = null;
     notifyListeners();
   }
 
+  // 위치 서비스 약관 체크박스 변경
+  void toggleGpsTerms(bool? value) {
+    _gpsTerms = value ?? false;
+    _allAgreed = _gpsTerms;
+    _errorMessage = null;
+    notifyListeners();
+  }
 }
