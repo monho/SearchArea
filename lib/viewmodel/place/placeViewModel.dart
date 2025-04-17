f8026e4e1f4f3a16bc0b5eddec3ace278f1162b3
MoonHo <75512752+monho@users.noreply.github.com>
2025-04-17 15:43:11 +0900

    feat : 네이버 위치 검색 API 호출 기능 완성

---
+++ lib/viewmodel/place/placeViewModel.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:searcharea/core/api_service.dart';
+import 'package:searcharea/model/place/place_model.dart';
+
+class PlaceViewModel with ChangeNotifier {
+  final NaverApiService _naverApiService = NaverApiService();
+  List<Place> _places = [];
+  bool _isLoading = false;
+  String _errorMessage = '';
+
+  List<Place> get places => _places;
+  bool get isLoading => _isLoading;
+  String get errorMessage => _errorMessage;
+
+  Future<void> fetchPlaces(String query) async {
+    _isLoading = true;
+    _errorMessage = '';
+    notifyListeners();
+
+    try {
+      _places = await _naverApiService.fetchPlaces(query);
+    } catch (e) {
+      _errorMessage = e.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  void toggleFavorite(int index) {
+    _places[index].isFavorite = !_places[index].isFavorite;
+    notifyListeners();
+  }
+}
